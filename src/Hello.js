2cd914c1b6e0d744c2172626acdb5ac688692e0e
Lukas saylor <lukassaylor@Lukass-Mac-mini.local>
2020-01-02 13:24:20 -0500

    initial upload

---
+++ src/Hello.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+              <h1>Stephs Buns</h1>
+              <p>worlds cutest Buns</p>
+              <br></br>
+              <h2>extra juicy</h2>
+            </div>
+        );        
+    }
+}
+
+export default Hello;
